270503ec6ea0231f94e9608ab9904881db640d2f
RawanMostafa08 <rawanmostafa401@gmail.com>
2025-08-21 15:08:14 +0300

    feata: handled array type

--- pkg/RESPParser.go
+++ pkg/RESPParser.go
@@ -42,6 +42,7 @@ func (b BulkStrings) Type() string   { return "BulkStrings" }
 func (b BulkStrings) String() string { return fmt.Sprintf("$%d\r\n%s\r\n", len(b.Value), b.Value) }
 
 type Array struct {
+	Length int
 	Values []RespType
 }
 
@@ -50,7 +51,7 @@ func (a Array) String() string {
 
 	s := fmt.Sprintf("*%d\r\n", len(a.Values))
 	for _, v := range a.Values {
-		s += v.String() + "\r\n"
+		s += v.String()
 	}
 	return s
 }
@@ -72,53 +73,81 @@ var ErrInvalidRESP = errors.New("invalid RESP syntax")
 var ErrUnkType = errors.New("unknown RESP type")
 
 func (p *Parser) parse(lines []string) error {
-	for i := 0; i < len(lines); i++ {
-		line := lines[i]
-		switch line[0] {
-		case '+': //simple String
-			p.RespList = append(p.RespList, SimpleString{Value: line[1:]})
-
-		case '-': //simple error
-			p.RespList = append(p.RespList, SimpleError{Value: line[1:]})
-
-		case ':': //integer
-			var value int
-			if _, err := fmt.Sscanf(line[1:], "%d", &value); err != nil {
-				return ErrInvalidRESP
-			}
-			p.RespList = append(p.RespList, Integer{Value: value})
-		case '$': //Bulk String $<length>\r\n<data>\r\n
-			var length int
-			if _, err := fmt.Sscanf(line[1:], "%d", &length); err != nil {
-				return ErrInvalidRESP
-			}
-
-			if length < 0 { // null bulk string
-				p.RespList = append(p.RespList, BulkStrings{Value: "", Length: -1})
-				continue
-			}
-
-			if length == 0 { // empty bulk string
-				p.RespList = append(p.RespList, BulkStrings{Value: "", Length: 0})
-				continue
-			}
-			value := lines[i+1]
-
-			if len(value) != length {
-				return ErrInvalidRESP
-			}
-
-			p.RespList = append(p.RespList, BulkStrings{Value: value, Length: length})
-			i++
-
-		default:
-			return ErrUnkType
+	for i := 0; i < len(lines); {
+		element, consumed, err := p.parseOne(lines, i)
+		if err != nil {
+			return err
 		}
+		p.RespList = append(p.RespList, element)
+		i += consumed
 	}
 	return nil
 }
 
-func (p *Parser) cleanLines(data string) []string {
+func (p *Parser) parseOne(lines []string, i int) (RespType, int, error) {
+	line := lines[i]
+	switch line[0] {
+	case '+': //simple String
+		return SimpleString{Value: line[1:]}, 1, nil
+
+	case '-': //simple error
+		return SimpleError{Value: line[1:]}, 1, nil
+
+	case ':': //integer
+		var value int
+		if _, err := fmt.Sscanf(line[1:], "%d", &value); err != nil {
+			return nil, 0, ErrInvalidRESP
+		}
+		return Integer{Value: value}, 1, nil
+	case '$': //Bulk String $<length>\r\n<data>\r\n
+		var length int
+		if _, err := fmt.Sscanf(line[1:], "%d", &length); err != nil {
+			return nil, 0, ErrInvalidRESP
+		}
+
+		if length < 0 { // null bulk string
+			return BulkStrings{Value: "", Length: -1}, 1, nil
+		}
+
+		if length == 0 { // empty bulk string
+			return BulkStrings{Value: "", Length: 0}, 1, nil
+		}
+		value := lines[i+1]
+
+		if len(value) != length {
+			return nil, 0, ErrInvalidRESP
+		}
+
+		return BulkStrings{Value: value, Length: length}, 2, nil
+
+	case '*': //Array *<count>\r\n<value1>\r\n<value2>\r\n...
+		var length int
+		if _, err := fmt.Sscanf(line[1:], "%d", &length); err != nil {
+			return nil, 0, ErrInvalidRESP
+		}
+
+		if length == 0 { // empty array
+			return Array{Values: make([]RespType, 0), Length: 0}, 1, nil
+		}
+
+		arr := Array{Values: make([]RespType, 0, length), Length: length}
+
+		consumed := 1 // already consumed the count line
+
+		for j := 0; j < length; j++ {
+			element, parsed, err := p.parseOne(lines, i+consumed)
+			if err != nil {
+				return nil, 0, err
+			}
+			arr.Values = append(arr.Values, element)
+			consumed += parsed
+		}
+		return arr, consumed, nil
+	}
+	return nil, 0, ErrUnkType
+}
+
+func cleanLines(data string) []string {
 	lines := strings.Split(string(data), "\n")
 
 	cleanedLines := make([]string, 0, len(lines))
@@ -143,7 +172,7 @@ func (p *Parser) LoadFromFile(path string) error {
 		return ErrEmptyFile
 	}
 
-	cleanedLines := p.cleanLines(string(data))
+	cleanedLines := cleanLines(string(data))
 	p.parse(cleanedLines)
 	return nil
 }
